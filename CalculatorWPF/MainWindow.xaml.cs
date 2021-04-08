cca070c91c24537f4aab50b26e230d1cb591addb
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-04-08 11:03:58 +0400

    Done ui design

---
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4A4A4A"));
+        }
+    }
+}
