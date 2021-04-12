09169bd826f31c0c202a589a4ff664f72f9676f9
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-04-12 12:56:01 +0400

    Update

--- CalculatorWPF/MainWindow.xaml.cs
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ExceptionServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CalculatorWPF
 {
@@ -21,17 +10,176 @@ namespace CalculatorWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Operation _operation;
+        private bool _firsInput = true;
+        private bool _textChanged;
+        private bool _calculated;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _operation = new Operation();
         }
 
-        private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        //private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    if (!(sender is Button button))
+        //        return;
+
+        //    button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4A4A4A"));
+        //}
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             if (!(sender is Button button))
                 return;
 
-            button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4A4A4A"));
+            if (_firsInput)
+            {
+                _textChanged = false;
+                _firsInput = false;
+                ClearInput();
+            }
+
+            AddValueToTextBlock(button.Content.ToString());
+        }
+
+        private void AddValueToTextBlock(string value)
+        {
+            if (value != "." && TextBlockValue.Text == "0")
+                TextBlockValue.Text = String.Empty;
+
+            var temp = TextBlockValue.Text + value;
+            TextBlockValue.Text = temp.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private bool CheckDot()
+        {
+            return TextBlockValue.Text.Contains(".");
+        }
+
+        private void ButtonDot_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (CheckDot())
+                return;
+
+            AddValueToTextBlock(".");
+        }
+
+        private void ButtonCe_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearInput();
+        }
+
+        private void ClearInput()
+        {
+            TextBlockValue.Text = "0";
+        }
+        private void ButtonDel_OnClick(object sender, RoutedEventArgs e)
+        {
+            RemoveValueFromTextBox();
+            TextBlockValue.Focus();
+
+            if (!_calculated)
+            {
+                _operation.Result = TextBlockValue.Text;
+            }
+        }
+
+        private void RemoveValueFromTextBox()
+        {
+            if (string.IsNullOrWhiteSpace(TextBlockValue.Text))
+                return;
+            
+            TextBlockValue.Text = TextBlockValue.Text.Remove(TextBlockValue.Text.Length - 1, 1);
+        }
+
+        private void ButtonPlusMinus_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (TextBlockValue.Text == "0")
+                return;
+
+            if (_calculated)
+                return;
+            var newValue = TextBlockValue.Text.StartsWith("-") ? 
+                TextBlockValue.Text.Remove(0, 1) : 
+                TextBlockValue.Text.Insert(0, "-");
+
+            TextBlockValue.Text = newValue.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void ButtonOperator_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            PerformOperation(button.Content.ToString());
+        }
+
+        private void PerformOperation(string oprt)
+        {
+            if (String.IsNullOrWhiteSpace(TextBlockValue.Text))
+                return;
+
+            if(String.IsNullOrWhiteSpace(_operation.FirstValue))
+            {
+                _operation.FirstValue = TextBlockValue.Text;
+                _firsInput = true;
+            }
+            else if (!_calculated && String.IsNullOrWhiteSpace(_operation.SecondValue))
+            {
+                Calculate();       
+            }
+
+            _operation.OperationType = _operation.GetOperationType(oprt);
+        }
+
+        private void Calculate()
+        {
+            if (!String.IsNullOrWhiteSpace(_operation.Result))
+            {
+                _operation.FirstValue = _operation.Result;
+            }
+
+            _operation.SecondValue = TextBlockValue.Text;
+
+            var result = String.Empty;
+
+            try
+            {
+                result = _operation.Calculate();
+            }
+            catch(Exception ex)
+            {
+                result = $"Error: {ex.Message}";
+            }
+
+            _operation.Result = result;
+
+            _operation.SecondValue = String.Empty;
+
+            TextBlockValue.Text = _operation.Result;
+
+            _calculated = true;
+            _firsInput = true;
+        }
+        private void TextBlockValue_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            _textChanged = true;
+            _calculated = false;
+        }
+
+        private void ButtonEqual_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!_calculated)
+                Calculate();
+        }
+
+        private void ButtonC_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearInput();
+            _operation.Clear();
         }
     }
 }
---
+++ CalculatorWPF/Operation.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculatorWPF
+{
+    public class Operation
+    {
+        public string FirstValue { get; set; }
+        public string SecondValue { get; set; }
+        public OperationType OperationType { get; set; }
+        public string Result { get; set; }
+
+        public Operation()
+        {
+            FirstValue = String.Empty;
+            SecondValue = String.Empty;
+        }
+
+        public void Clear()
+        {
+            FirstValue = String.Empty;
+            SecondValue = String.Empty;
+            Result = String.Empty;
+        }
+        public string Calculate()
+        {
+            if (String.IsNullOrWhiteSpace(FirstValue))
+                return String.Empty;
+
+            if (String.IsNullOrWhiteSpace(SecondValue))
+                return FirstValue;
+
+
+            var firstValue = Double.Parse(FirstValue);
+            var secondValue = Double.Parse(SecondValue);
+            double result;
+            switch (OperationType)
+            {
+                case OperationType.Add:
+                    result = firstValue + secondValue;
+                    break;
+                case OperationType.Sub:
+                    result = firstValue - secondValue;
+                    break;
+                case OperationType.Mul:
+                    result = firstValue * secondValue;
+                    break;
+                case OperationType.Div:
+                {
+                    if (secondValue == 0)
+                        throw new DivideByZeroException("Divide by zero");
+                    
+                    result = firstValue / secondValue;
+                }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return result.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public OperationType GetOperationType(string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return OperationType.Add;
+                case "-":
+                    return OperationType.Sub;
+                case "*":
+                    return OperationType.Mul;
+                case "/":
+                    return OperationType.Div;
+                case "=":
+                    return OperationType.Equal;
+                default:
+                    throw new InvalidOperationException("Unknown operation keyword");
+            }
+        }
+    }
+
+    public enum OperationType
+    {
+        Add,
+        Sub,
+        Mul,
+        Div,
+        Equal
+    }
+}
